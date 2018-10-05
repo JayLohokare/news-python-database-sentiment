e130f1ede153a555c80ca053043e1ca6c69e1cad
Jay Lohokare <jaylohokare@gmail.com>
2018-10-05 15:02:49 -0400

    Added NewAPI library, from-to filters, source based fetch

--- Mongo/getNews.sh
+++ Mongo/getNews.sh
@@ -1,1 +1,9 @@
-python3 fetchNewsPython.py
+Interactive 3d talking model (chat-bot) powered by Computer Vision and AI. \\ 
+
+Chat-bots are no longer boring! A 3d rendered model with lifelike expressions and actions that talks with people standing in front of the screen. Highly contextual and personalised interactions powered by data captured by Computer Vision, BLE beacons and data APIs. \ 
+
+Users can talk and interact with the system, get data they need, get recommendations and suggestions through the various APIs that power the AI. \\ 
+
+Computer Vision detects patterns in the images captured. BLE pushes data from smartphone applications to the system to result in a targeted data transfer. \ 
+
+The Virtual assistant is rendered on a browser-based Unity3d application. The expressions of the virtual assistant (Lip movement, expressions and face movements) are driven by Deep learning (Neural networks). The chatbot is driven by rules based conversation agent getting input from voice (Users) and various APIs feeding the data into the system.
